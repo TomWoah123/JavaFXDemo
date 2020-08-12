95e3c27bbfb9cdc53a86bd887d21250e95981d96
Timothy Wu <timothywu02@gmail.com>
2020-08-12 10:47:12 -0400

    Learning JavaFX

---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        StackPane root = new StackPane();
+        Button button = new Button();
+        button.setText("Click on me");
+        root.getChildren().add(button);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
